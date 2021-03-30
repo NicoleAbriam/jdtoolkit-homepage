5b6fcdac760edd9274859a9014a038e9cdcc431f
Nicole <77802525+NicoleAbriam@users.noreply.github.com>
2021-03-30 13:39:55 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,16 +7,3 @@ $('ul li a').hover(function() {
         'left' : x.position().left
      }, 400);
 });
-
-// SLIDESHOW
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
-
